332f1223ea631185337d2949196ad2047b946c7b
kristianmandrup <kmandrup@gmail.com>
2023-07-19 20:16:42 +0200

    Kind of working

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -7,42 +7,50 @@
 			title: 'Add new feature',
 			description: 'This is a new feature that we need to add to the product.',
 			dateLastChanged: new Date(),
-			type: 'frontend'
+			type: 'Todo'
 		},
 		{
 			title: 'Fix bug in search bar',
 			description:
 				'There is a bug in the search bar that is preventing users from finding what they are looking for.',
 			dateLastChanged: new Date(),
-			type: 'backend'
+			type: 'Done'
 		},
 		{
 			title: 'Test new feature',
 			description: 'We need to test the new feature to make sure that it is working correctly.',
 			dateLastChanged: new Date(),
-			type: 'test'
+			type: 'In Progress'
 		}
 	];
 
-	// turns the list into a map
-	// test: [{task 1 }, { task 2}]
-	const tasksByColumn: Record<string, ITask[]> = tasks.reduce((acc, task: ITask) => {
-		acc[task.type] = acc[task.type] || [];
-		acc[task.type].push(task);
+	const columns = ['Todo', 'In Progress', 'Done'];
+
+	const taskMap = columns.reduce((acc, col: string) => {
+		acc[col] = [];
 		return acc;
 	}, {} as Record<string, ITask[]>);
 
-	console.log({ tasksByColumn });
+	console.log({ taskMap });
 
-	const columns = ['Todo', 'In Progress', 'Done'];
+	// turns the list into a map
+	// test: [{task 1 }, { task 2}]
+	const tasksByColumn = tasks.reduce((acc, task: ITask) => {
+		acc[task.type].push(task);
+		return acc;
+	}, taskMap);
+
+	console.log({ tasksByColumn });
 </script>
 
 <div class="board">
 	<div class="tasks">
 		{#each columns as column}
+			{column}
 			<div class="column">
 				{#each tasksByColumn[column] as task}
-					<Task {task} />
+					{JSON.stringify(task, null, 2)}
+					<!-- <Task {task} /> -->
 				{/each}
 			</div>
 		{/each}
