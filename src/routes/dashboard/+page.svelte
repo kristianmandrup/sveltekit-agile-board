1f5780330bbcafd392e5b5c9eeeeca790835a477
kristianmandrup <kmandrup@gmail.com>
2023-07-21 15:23:05 +0200

    Initial pages and layout

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { Bar, Doughnut } from 'svelte-chartjs';
+
+	let taskStatusData: any = {};
+	let projectProgressData: any = {};
+
+	onMount(() => {
+		taskStatusData = {
+			labels: ['To Do', 'In Progress', 'Blocked', 'Completed'],
+			datasets: [
+				{
+					label: 'Task Status',
+					data: [10, 5, 3, 7], // Replace these values with your actual data
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)'
+					],
+					borderWidth: 1
+				}
+			]
+		};
+
+		projectProgressData = {
+			labels: ['Completed', 'Remaining'],
+			datasets: [
+				{
+					data: [25, 75], // Replace these values with your actual data
+					backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+					borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
+					borderWidth: 1
+				}
+			]
+		};
+	});
+</script>
+
+<layout>
+	<div class="chart-container">
+		<h2>Task Status</h2>
+		<Bar data={taskStatusData} options={{ responsive: true, maintainAspectRatio: false }} />
+	</div>
+
+	<div class="chart-container">
+		<h2>Project Progress</h2>
+		<Doughnut
+			data={projectProgressData}
+			options={{ responsive: true, maintainAspectRatio: false }}
+		/>
+	</div>
+</layout>
+
+<style>
+	.chart-container {
+		max-width: 600px;
+		margin: 1rem auto;
+		height: 400px;
+	}
+</style>
---
+++ src/routes/project/create/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	let name = '';
+	let description = '';
+	let motivation = '';
+	let targetUsers = '';
+
+	function handleSubmit() {
+		// Here you would usually send the form data to your backend
+		console.log({ name, description, motivation, targetUsers });
+
+		// After submitting the form, redirect to the team creation page
+		goto('/team_creation');
+	}
+</script>
+
+<layout>
+	<form on:submit|preventDefault={handleSubmit}>
+		<label>
+			Project Name:
+			<input bind:value={name} required />
+		</label>
+		<label>
+			Description:
+			<textarea bind:value={description} required />
+		</label>
+		<label>
+			Motivation:
+			<textarea bind:value={motivation} required />
+		</label>
+		<label>
+			Target Users:
+			<input bind:value={targetUsers} required />
+		</label>
+		<button type="submit">Create Project</button>
+	</form>
+</layout>
